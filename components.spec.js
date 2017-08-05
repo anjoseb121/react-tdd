6cec3cb4bf20ee26f379c6b66df4a12b19b61f4c
Antonio J. Bar <anjose.121@gmail.com>
2017-08-05 17:31:09 -0500

    Add input area, beerlist and empty list tests

--- components.spec.js
+++ components.spec.js
@@ -1,8 +1,20 @@
+import React from 'react';
 import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
+import { BeerListContainer } from './components';
+import { InputArea, BeerList } from './components';
 
+describe('BeerListContainer', () => {
+  it('should render InputArea and BeerList', () => {
+    const wrapper = shallow(<BeerListContainer/>);
+    expect(wrapper.containsAllMatchingElements([
+      <InputArea/>,
+      <BeerList/>
+    ])).to.equal(true);
+  });
 
-describe('the environment', () => {
-    it('works, hopefully', () => {
-        expect(true).to.be.true;
-    })
-})
+  it('should start with an empty list', () => {
+      const wrapper = shallow(<BeerListContainer/>);
+      expect(wrapper.state('beers')).to.eql([]);
+  })
+});
